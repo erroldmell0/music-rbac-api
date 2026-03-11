381ed52d023acc58c1de2081dbe9d6fcffb8f5e7
Errol D'mello <erroldmello2005@gmail.com>
2026-03-11 22:22:32 +0530

    feat: completed album controller

--- src/controllers/music.controller.js
+++ src/controllers/music.controller.js
@@ -1,4 +1,5 @@
 const musicModel = require('../models/music.model');
+const albumModel = require('../models/album.model');
 const uploadFile = require('../services/storage.service');
 const jwt = require('jsonwebtoken');
 
@@ -40,7 +41,37 @@ async function createMusic(req, res) {
 
 }
 
+async function createAlbum(req, res) {
+    const token = req.cookies.token;
+
+    if (!token) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+    try{
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        if(decoded.role !== 'artist') {
+            return res.status(403).json({ message: 'You do not have permission to perform this action' });
+        }
+
+        const { title, musicIds } = req.body;
+
+        const album = await albumModel.create({
+            title,
+            musics: musicIds,
+            artist: decoded.id,
+        });
+
+        res.status(201).json({
+            message: 'Album created successfully',
+            album
+        });
+    } catch (err) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+}
 
 module.exports = {
-    createMusic
+    createMusic,
+    createAlbum
 };
