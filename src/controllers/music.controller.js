04cbfa9face050e93ceed9697aaff3c8dbc34d5e
Errol D'mello <erroldmello2005@gmail.com>
2026-03-14 00:17:06 +0530

    feat: implement artist authentication middleware and secure music upload routes

--- src/controllers/music.controller.js
+++ src/controllers/music.controller.js
@@ -5,71 +5,38 @@ const jwt = require('jsonwebtoken');
 
 
 async function createMusic(req, res) {
+   const { title } = req.body;
+    const file = req.file;
 
-    const token = req.cookies.token;
+    const result = await uploadFile(file.buffer.toString('base64'));
 
-    if (!token) {
-        return res.status(401).json({ message: 'Unauthorized' });
-    }
+    const music = await musicModel.create({
+        uri: result.url,
+        title,
+        artist: req.user.id,
+    });
 
-    try{
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        if(decoded.role !== 'artist') {
-            return res.status(403).json({ message: 'You do not have permission to perform this action' });
-        }
-    
-        const { title } = req.body;
-        const file = req.file;
-
-        const result = await uploadFile(file.buffer.toString('base64'));
-
-        const music = await musicModel.create({
-            uri: result.url,
-            title,
-            artist: decoded.id,
-        });
-
-        res.status(201).json({
-            message: 'Music created successfully',
-            music
-        });
-
-    } catch (err) {
-    console.error('Create music error:', err);
-    return res.status(401).json({ message: 'Unauthorized', error: err.message });
-}
-
+    res.status(201).json({
+        message: 'Music created successfully',
+        music
+    });
 }
 
 async function createAlbum(req, res) {
-    const token = req.cookies.token;
+    const { title, musics } = req.body;
 
-    if (!token) {
-        return res.status(401).json({ message: 'Unauthorized' });
-    }
+    const album = await albumModel.create({
+        title,
+        musics: musics,
+        artist: req.user.id,
+    });
 
-    try{
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        if(decoded.role !== 'artist') {
-            return res.status(403).json({ message: 'You do not have permission to perform this action' });
-        }
-
-        const { title, musics } = req.body;
-
-        const album = await albumModel.create({
-            title,
-            musics: musics,
-            artist: decoded.id,
-        });
-
-        res.status(201).json({
-            message: 'Album created successfully',
-            album
-        });
-    } catch (err) {
-        return res.status(401).json({ message: 'Unauthorized' });
-    }
+    res.status(201).json({
+        message: 'Album created successfully',
+        album
+    });
 }
+
 
 module.exports = {
     createMusic,
